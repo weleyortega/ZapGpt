d40b3ac10bde78655e8d854876f98d08cb42632a
webster-rafael <webster.rafael2014@gmail.com>
2024-11-28 17:44:19 -0400

    Adicionado: Tags do meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import Script from "next/script";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +26,38 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Script
+          id="meta-pixel-script" // Atribuindo um id único ao script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq) return;
+                n=f.fbq=function(){n.callMethod ?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq) f._fbq=n;
+                n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '554340387551745'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=554340387551745&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
